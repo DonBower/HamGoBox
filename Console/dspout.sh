f90fd29bbf19a9ba86dfa2810c59a342558b61b3
Don Bower <don.bower@outlook.com>
2021-03-06 11:21:06 -0800

    add gitpull to boot

--- Console/dspout.sh
+++ Console/dspout.sh
@@ -4,6 +4,21 @@ if [[ -f /home/pi/dspout.py.errlog.txt ]]; then
   rm /home/pi/dspout.py.errlog.txt
 fi
 
+touch /home/pi/gitpull.log
+chmod 666 /home/pi/gitpull.log
+
+date >> /home/pi/gitpull.log
+
+pushd /home/pi/Developer/HamGoBox
+
+if nc -tv -w 2 github.com 443; then
+  git pull >> /home/pi/gitpull.log 2>&1
+else
+  echo github not avaiable >> /home/pi/gitpull.log 2>&1
+fi
+
+popd
+
 while true; do
   if ps -ef | grep dspout.py | grep --quiet -v grep; then
     sleep 10
