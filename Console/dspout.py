30b5ce1ea8711c8ca37112f6f8b8019dd37e83a3
Don Bower <don.bower@outlook.com>
2021-05-25 14:52:59 -0700

    update output

--- Console/dspout.py
+++ Console/dspout.py
@@ -1,9 +1,12 @@
-#!//usr/bin/python3
+#!/usr/bin/python3
 import os
 from os import path
 import sys
 import time
 from datetime import datetime
+from datetime import timedelta
+import requests
+import xml.etree.ElementTree as ET
 import json
 import board
 import busio
@@ -13,6 +16,10 @@ import adafruit_hts221
 import adafruit_ltr390
 import adafruit_tsl2591
 import adafruit_scd30
+
+xmlFile = '/tmp/solarrss.xml'
+now = datetime.now()
+freshDate = now + timedelta(hours = 12)
 
 #last_print = time.monotonic()
 
@@ -357,10 +364,66 @@ def printSCD():
         thisCO2Lvl   = '.....(Low)'
     print(f'CO2......{thisCO2Lvl:10s}: {thisCO2:3,.1f}ppm                   '[:40], end='')
 
+def bandIndex(thisBand):
+    switcher={
+        '80m-40m':0,
+        '30m-20m':1,
+        '17m-15m':2,
+        '12m-10m':3
+        }
+    return switcher.get(thisBand,"Invalid Band")
+
+def timeIndex(thisTime):
+    switcher={
+            'day':0,
+            'night':1
+         }
+    return switcher.get(thisTime,"Invalid Time")
+
+def getRSS(xmlFile):
+    # url of rss feed
+    url = 'http://www.hamqsl.com/solarrss.php'
+    # creating HTTP response object from given url
+    resp = requests.get(url)
+    # saving the xml file
+    with open(xmlFile, 'wb') as f:
+        f.write(resp.content)
+
+def parseRSS(xmlFile):
+    global thisMUF, theseConditions, freshDate
+    # create element tree object
+    tree = ET.parse(xmlFile)
+    # get root element
+    root = tree.getroot()
+
+    fileDate=root.findall('./channel/item/solar/solardata/updated')[0].text
+    rssDate=datetime.strptime(fileDate," %d %b %Y %H%M %Z")
+    freshDate = rssDate + timedelta(hours = 12)
+
+    solarData = root.findall('./channel/item/solar/solardata')
+    thisMUF=root.findall('./channel/item/solar/solardata/muf')[0].text
+    # create empty list for news items
+    theseConditions = [['' for x in range(4)] for y in range(2)]
+    # iterate news items
+    for band in root.findall('./channel/item/solar/solardata/calculatedconditions/band'):
+        thisBand=band.attrib['name']
+        thisTime=band.attrib['time']
+        thisCondition=band.text
+        theseConditions[timeIndex(thisTime)][bandIndex(thisBand)] = thisCondition
+
+def printSolar():
+    print(f'MUF={thisMUF:5s}|80m-40m|30m-20m|17m-15m|12m-10m                    '[:40])
+    print(f'Day      |{theseConditions[0][0]:7s}|{theseConditions[0][1]:7s}|{theseConditions[0][2]:7s}|{theseConditions[0][3]:7s}                    '[:40])
+    print(f'Night    |{theseConditions[1][0]:7s}|{theseConditions[1][1]:7s}|{theseConditions[1][2]:7s}|{theseConditions[1][3]:7s}                    '[:40])
+
+if not path.exists(xmlFile):
+    getRSS(xmlFile)
+
 clearScreen()
 while True:
 #    sys.stdout.write("\x1b[2J\x1b[H") # Position to top
     sys.stdout.write("\x1b[H") # Position to top
+    now = datetime.now()
     printTimeStamp()
     if not divmod(int(datetime.now().strftime("%S")),6)[1]:
         if hasGPS:
@@ -377,4 +440,8 @@ while True:
 #            printLTR()
 #        if hasTSL:
 #            printTSL()
+    if now > freshDate:
+        getRSS(xmlFile)
+        parseRSS(xmlFile)
+    printSolar()
     time.sleep(1)
