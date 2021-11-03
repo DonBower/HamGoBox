c0cbd1a71f01ee2238e8391dfd9c15816e067f56
Don Bower <don.bower@outlook.com>
2021-11-03 13:40:29 -0700

    add debug

--- Console/dspkill.sh
+++ Console/dspkill.sh
@@ -3,8 +3,10 @@ clear
 echo ">>>>>Kill DSPOUT<<<<<<< "
 sh=`ps -ef | grep dspout.sh | grep errlog | awk '{print $2}'`
 py=`ps -ef | grep dspout.py | grep errlog | awk '{print $2}'`
+echo -e "sudo kill -9 ${sh}"
 sudo kill -9 ${sh}
 sleep 1
+echo -e "sudo kill -9 ${py}"
 sudo kill -9 ${py}
 sleep 1
 clear
