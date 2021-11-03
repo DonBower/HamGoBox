89dea44ed9245390cf1910e6cefadee425d19a26
Don Bower <don.bower@outlook.com>
2021-11-03 13:43:51 -0700

    fix grep

--- Console/dspkill.sh
+++ Console/dspkill.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 clear
 echo ">>>>>Kill DSPOUT<<<<<<< "
-sh=`ps -ef | grep dspout.sh | grep errlog | awk '{print $2}'`
-py=`ps -ef | grep dspout.py | grep errlog | awk '{print $2}'`
+sh=`ps -ef | grep dspout.sh | grep HamGoBox | awk '{print $2}'`
+py=`ps -ef | grep dspout.py | grep HamGoBox | awk '{print $2}'`
 echo -e "sudo kill -9 ${sh}"
 sudo kill -9 ${sh}
 sleep 1
