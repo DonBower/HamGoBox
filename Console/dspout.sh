f7af13f4738cfa5c3b92b9e8573fadcc8b0b45c1
Don Bower <don.bower@outlook.com>
2026-05-23 12:16:21 -0700

    add some messages

--- Console/dspout.sh
+++ Console/dspout.sh
@@ -10,15 +10,21 @@ chmod 666 /home/don/gitpull.log
 date >> /home/don/gitpull.log
 
 while true; do
-  if ps -ef | grep dspout.py | grep --quiet -v grep; then
+  if ps -ef | grep dspout.py | grep --quiet -v grep; then    
     sleep 10
   else
+    printf '\033[3J\033[2J\033[H' > /dev/tty1
+    echo "application not running wait 10 seconds" >> /dev/tty1
+    sleep 10
+
     pushd /home/don/Developer/HamGoBox
 
     if nc -tv -w 10 github.com 443; then
       git pull >> /home/don/gitpull.log 2>&1
     else
+      echo github not avaiable >> /dev/tty1
       echo github not avaiable >> /home/don/gitpull.log 2>&1
+      sleep 1
     fi
 
     popd
